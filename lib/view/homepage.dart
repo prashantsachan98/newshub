81e62e6eefff9e3403321146943561dc3cb02b0e
prashantsachan98 <prashantsachan03@gmail.com>
2021-08-05 10:59:01 +0530

    screenshots

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final List data = [
     'all_news',
     'trending',
-    'top stories',
+    'top_stories',
     'national',
     'business',
     'politics',
